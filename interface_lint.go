2cbf99b44fdfa26153cf18f7f61f93642ff6ce4c
wwbweibo <wwbweibo@outlook.com>
2023-09-25 18:19:02 +0800

    fix error

--- interface_lint.go
+++ interface_lint.go
@@ -7,6 +7,9 @@ import (
 
 func lintInterface(t *ast.TypeSpec, i *ast.InterfaceType, pass analysis.Pass, currentVersion string) {
 	for _, f := range i.Methods.List {
+		if f.Doc == nil {
+			return
+		}
 		for _, c := range f.Doc.List {
 			commonLint(f.Pos(), pass, t.Name.Name+"."+f.Names[0].Name, c.Text, currentVersion)
 		}
--- struct_lint.go
+++ struct_lint.go
@@ -7,6 +7,9 @@ import (
 
 func lintStruct(t *ast.TypeSpec, s *ast.StructType, pass analysis.Pass, currentVersion string) {
 	for _, f := range s.Fields.List {
+		if f.Doc == nil {
+			return
+		}
 		for _, c := range f.Doc.List {
 			commonLint(f.Pos(), pass, t.Name.Name+"."+f.Names[0].Name, c.Text, currentVersion)
 		}
